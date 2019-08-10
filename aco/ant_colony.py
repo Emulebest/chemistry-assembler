d491b1336fa01890bf156e09797f2ae321cb1d57
emulebest <igorkorotach@gmail.com>
2019-08-10 11:39:36 +0300

    added real data set & some typing stuff

--- aco/ant_colony.py
+++ aco/ant_colony.py
@@ -1,4 +1,3 @@
-import random as rn
 from typing import List, Tuple
 
 import numpy as np
@@ -8,7 +7,8 @@ from sklearn import linear_model
 
 class BinaryFeatureSelectionAntColony:
 
-    def __init__(self, n_ants, n_iterations, decay, data_set, score_weight=1, count_weight=1):
+    def __init__(self, n_ants: int, n_iterations: int, decay: float, data_set: List[np.ndarray], score_weight=1,
+                 count_weight=1):
         """
         Args:
             construction_matrix (2D numpy.array): Square matrix of distances. Diagonal is assumed to be np.inf.
@@ -28,10 +28,10 @@ class BinaryFeatureSelectionAntColony:
         self.score_weight = score_weight
         self.count_weight = count_weight
         self.data_set = data_set
-        self.current_iteration = 1
-        self.times_taken = np.ones((2, len(self.data_set) - 1))
-        self.pheromone = np.ones((2, len(self.data_set) - 1))
-        self.construction_matrix = np.zeros([2, len(self.data_set) - 1])
+        self.current_iteration: int = 1
+        self.times_taken: np.ndarray = np.ones((2, len(self.data_set) - 1))
+        self.pheromone: np.ndarray = np.ones((2, len(self.data_set) - 1))
+        self.construction_matrix: np.ndarray = np.zeros([2, len(self.data_set) - 1])
         for i in range(len(self.construction_matrix[1])):
             self.construction_matrix[1][i] = 1
 
@@ -43,8 +43,8 @@ class BinaryFeatureSelectionAntColony:
             shortest_path = max(all_paths, key=lambda x: x[1])
             print(shortest_path)
             if shortest_path[1] > all_time_shortest_path[1] or (
-                    shortest_path[1] == all_time_shortest_path[1] and self.calculate_feature_amount(
-                    shortest_path[0]) < self.calculate_feature_amount(all_time_shortest_path[0])):
+                    shortest_path[1] == all_time_shortest_path[1] and self.calculate_feature_amount(shortest_path[0]) <
+                    self.calculate_feature_amount(all_time_shortest_path[0])):
                 all_time_shortest_path = shortest_path
             print(self.current_iteration)
             self.current_iteration += 1
@@ -53,15 +53,7 @@ class BinaryFeatureSelectionAntColony:
     def calculate_feature_amount(self, path: List[Tuple[int, int]]) -> int:
         return len(list(filter(lambda x: x == 1, map(lambda x: x[1], path))))
 
-    def spread_pheronome(self, all_paths):
-        # heuristric_sum = 0
-        # for path, score in all_paths:
-        #     count = self.calculate_feature_amount(path)
-        #     try:
-        #         heuristric_sum += score / count
-        #     except ZeroDivisionError:
-        #         pass
-        # total_sum = (1 - self.decay) * heuristric_sum
+    def spread_pheronome(self, all_paths: List[Tuple[List[Tuple[int, int]], float]]):
         for path, score in all_paths:
             count = self.calculate_feature_amount(path)
             for i, chosen in path:
@@ -70,26 +62,22 @@ class BinaryFeatureSelectionAntColony:
                 self.pheromone[chosen][i] = (1 - self.decay) * self.pheromone[chosen][i] + (
                         score / count ** self.count_weight) * self.score_weight
                 self.times_taken[chosen][i] += 1
-        #
-        # for i in range(len(self.pheromone[0])):
-        #     self.pheromone[0][i] = (1 - self.decay) * self.pheromone[0][i] + total_sum
-        #     self.pheromone[1][i] = (1 - self.decay) * self.pheromone[1][i] + total_sum
 
-    def gen_all_paths(self):
+    def gen_all_paths(self) -> List[Tuple[List[Tuple[int, int]], float]]:
         all_paths = []
         for i in range(self.n_ants):
             path = self.gen_path()
             all_paths.append((path, self.calculate_svm(path)))
         return all_paths
 
-    def gen_path(self):
+    def gen_path(self) -> List[Tuple[int, int]]:
         path = []
         for i in range(len(self.construction_matrix[0])):
             move: int = self.pick_move(self.pheromone[0][i], self.pheromone[1][i], i)
             path.append((i, move))
         return path
 
-    def pick_move(self, pheromone_exclude, pheromone_include, i):
+    def pick_move(self, pheromone_exclude: np.ndarray, pheromone_include: np.ndarray, i: int) -> int:
 
         excluded = pheromone_exclude * (self.times_taken[0][i] / (self.n_ants * self.current_iteration))
         included = pheromone_include * (self.times_taken[1][i] / (self.n_ants * self.current_iteration))
@@ -99,7 +87,7 @@ class BinaryFeatureSelectionAntColony:
         move = np_choice([0, 1], 1, p=rows)[0]
         return move
 
-    def calculate_svm(self, path) -> float:
+    def calculate_svm(self, path: List[Tuple[int, int]]) -> float:
         chosen_features = []
         for i, chosen in path:
             if chosen:
--- aco/ant_colony_test.py
+++ aco/ant_colony_test.py
@@ -1,7 +1,5 @@
 import unittest
-from typing import List
-
-import numpy as np
+import pandas as pd
 
 from aco.ant_colony import BinaryFeatureSelectionAntColony
 
@@ -9,17 +7,21 @@ from aco.ant_colony import BinaryFeatureSelectionAntColony
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.featureA = np.random.randint(10000, size=(200, 1))
-        self.featureB = np.random.randint(10000, size=(200, 1))
-        self.featureC = np.random.randint(10000, size=(200, 1))
-        self.featureD = np.random.randint(10000, size=(200, 1))
-        self.featureE = np.random.randint(10000, size=(200, 1))
-        self.featureF = np.random.randint(10000, size=(200, 1))
-        self.result = np.random.randint(10000, size=(200, 1))
+        frame = pd.read_csv('../data/Admission_Predict_Ver1.1.csv')
+        self.featureA = frame.iloc[:, 1:2].to_numpy()
+        self.featureB = frame.iloc[:, 2:3].to_numpy()
+        self.featureC = frame.iloc[:, 3:4].to_numpy()
+        self.featureD = frame.iloc[:, 4:5].to_numpy()
+        self.featureE = frame.iloc[:, 5:6].to_numpy()
+        self.featureF = frame.iloc[:, 6:7].to_numpy()
+        self.featureG = frame.iloc[:, 7:8].to_numpy()
+        self.featureH = frame.iloc[:, 8:9].to_numpy()
+        self.result = frame.iloc[:, len(frame.columns) - 1].to_numpy()
 
     def test_ACO(self):
-        data_set = [self.featureA, self.featureB, self.featureC, self.featureD, self.featureE, self.featureF, self.result]
-        ant_colony = BinaryFeatureSelectionAntColony(2, 4, 0.05, data_set, 2, 10)
+        data_set = [self.featureA, self.featureB, self.featureC, self.featureD, self.featureE, self.featureF,
+                    self.featureG, self.featureH, self.result]
+        ant_colony = BinaryFeatureSelectionAntColony(3, 4, 0.05, data_set, 2, 25)
         result_path = ant_colony.run()
         print(f"Result {result_path}")
         self.assertGreaterEqual(result_path[1], 0.99)
